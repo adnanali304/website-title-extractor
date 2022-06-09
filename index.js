85c63944bece5efb2ffcc76941db81869cb36ae9
adnanali304 <70511705+adnanali304@users.noreply.github.com>
2022-06-09 18:06:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,13 @@ const express = require("express")
 const app = express();
 const _webpage =  require("./libs/webpage");
 
-
 app.set('view engine', 'ejs');
-
-
 app.get("/i/want/title", async (req,res) => {
     let {address} = req.query;
     if(typeof address !== "object") address = [address];
     const result = await Promise.all(address.map(address => _webpage.extractTitle(address)));
     res.render('home', {result: result});
 });
-
 
 app.use((req, res) => {
     res.status(404).end('404 - Not Found!');
